69c69b5f3f2a7f2b09633a23a8cb829716927fce
estiven-piedrahita <duvanestivenpiedrahita@gmail.com>
2026-04-29 14:34:01 -0500

    feat: All the services finished successfully

---
+++ Interfaces/IAdvisorService.cs
@@ -0,0 +1,13 @@
+﻿using BankTurns.Models;
+using BankTurns.Response;
+
+namespace BankTurns.Interfaces
+{
+    public interface IAdvisorService
+    {
+        Task<ServicesResponse<Advisor>>       CreateAsync(string name, string email, string password);
+        Task<ServicesResponse<Advisor>>       LoginAsync(string email, string password);
+        Task<ServicesResponse<List<Advisor>>> GetAllActiveAsync();
+        Task<ServicesResponse<Advisor>>       ToggleStatusAsync(int advisorId);
+    }
+}
--- Interfaces/ITurnService.cs
+++ Interfaces/ITurnService.cs
@@ -1,13 +1,18 @@
 ﻿using BankTurns.Models;
+using BankTurns.Response;
 
-namespace BankTurns.Interfaces;
-
-public interface ITurnService
+namespace BankTurns.Interfaces
 {
-    Task<Turn> CreateAsync(int userId);
-    Task<List<Turn>> GetQueueAsync();
-    Task<Turn?> GetCurrentTurnAsync(int advisorId);
-    Task<Turn?> CallNextAsync(int advisorId);
-    Task<Turn?> FinishTurnAsync(int advisorId, string? comment);
-    Task<bool> HasActiveTurnAsync(int userId);
+    public interface ITurnService
+    {
+        Task<ServicesResponse<Turn>>       CreateAsync(int userId);
+        Task<ServicesResponse<List<Turn>>> GetQueueAsync();
+        Task<ServicesResponse<Turn>>       CallNextAsync(int advisorId);
+        Task<ServicesResponse<Turn>>       FinishTurnAsync(int advisorId, string? comment);
+        Task<ServicesResponse<Turn>>       HasActiveTurnAsync(int userId);
+        Task<ServicesResponse<List<Turn>>> GetAdvisorTurnsAsync(int advisorId);
+        Task<ServicesResponse<Turn>>       CancelTurnAsync(int userId);
+        Task<ServicesResponse<TicketDto>> GetTicketAsync(int turnId);
+
+    }
 }
